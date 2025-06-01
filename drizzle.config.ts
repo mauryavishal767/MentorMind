b2edb10607b32c28a86aa9cdc21ce0a3b193d59f
Vishal Maurya <mauryavishal767@gmail.com>
2025-06-01 21:19:41 +0530

    added getEnv function

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,26 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+function getEnv(key: string): string {
+    const val = process.env[key];
+    if (!val) throw new Error(`Missing required env var: ${key}`);
+    return val;
+}
+
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT!),
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    ssl: "require",
+    host: getEnv('DB_HOST'),
+    port: Number(getEnv('DB_PORT')),
+    user: getEnv('DB_USER'),
+    password: getEnv('DB_PASSWORD'),
+    database: getEnv('DB_NAME'),
+    ssl:
+      getEnv('DB_SSL') === 'true'
+        ? { rejectUnauthorized: true }
+        : false,
   },
 });
 
